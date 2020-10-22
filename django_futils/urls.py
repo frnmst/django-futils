3d6c1d810916d824f232c5b5cdc1e523bd5578e4
Franco Masotti <franco.masotti@live.com>
2020-10-22 18:07:16 +0200

    Minor fixes.

--- django_futils/urls.py
+++ django_futils/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path, re_path
+from django.contrib import admin
+from django.urls import path, re_path, reverse_lazy, include
+# See https://overiq.com/django-1-10/handling-media-files-in-django/
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
-# from . import views
-
+# See https://stackoverflow.com/a/55723121
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path(r'^$', RedirectView.as_view(url=reverse_lazy('admin:index'))),
 ]
