ec524a4bbf0b22eeefddc73718a3f5c532e976ad
Franco Masotti <franco.masotti@live.com>
2020-12-05 18:49:28 +0100

    Updated version number.

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ author = 'Franco Masotti'
 # built documents.
 #
 # The short X.Y version.
-version = '0.0.0'
+version = '0.1.0'
 # The full version, including alpha/beta/rc tags.
-release = '0.0.0'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 
