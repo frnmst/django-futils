545b95b646723fe14c4f0d6868b83d4f01919429
Franco Masotti <franco.masotti@live.com>
2020-12-12 23:33:42 +0100

    Added permissions for views.

--- django_futils/views.py
+++ django_futils/views.py
@@ -1,28 +1,40 @@
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .default_models import AddressType, Person, PersonAddress, Company, Municipality
 
 
-class PersonView(generic.DetailView):
+class BasePermissions(LoginRequiredMixin):
+    r"""See
+        https://docs.djangoproject.com/en/3.1/topics/auth/default/#the-loginrequired-mixin
+    """
+    login_url = '/admin/login/'
+    redirect_field_name = 'next'
+
+
+class PersonView(BasePermissions, generic.DetailView):
     model = Person
     template_name = 'django_futils/person_object.html'
 
 
-class PersonAddressView(generic.DetailView):
+class PersonAddressView(BasePermissions, generic.DetailView):
     model = PersonAddress
     template_name = 'django_futils/personaddress_object.html'
 
 
-class CompanyView(generic.DetailView):
+class CompanyView(BasePermissions, generic.DetailView):
     model = Company
     template_name = 'django_futils/company_object.html'
 
 
-class MunicipalityView(generic.DetailView):
+class MunicipalityView(BasePermissions, generic.DetailView):
     model = Municipality
     template_name = 'django_futils/municipality_object.html'
 
 
-# Type views.
-class AddressTypeView(generic.DetailView):
+##############
+# Type views #
+##############
+class AddressTypeView(BasePermissions, generic.DetailView):
     model = AddressType
     template_name = 'django_futils/addresstype_object.html'
